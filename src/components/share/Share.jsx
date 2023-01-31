cf823f30695adc1295a016902c29253c47ba227e
Tajeddine BOURHIM <bourhimtajeddine@gmail.com>
2023-01-31 20:56:11 +0100

    Project Complete

---
+++ src/components/share/Share.jsx
@@ -0,0 +1,43 @@
+import "./share.css";
+import PermMediaIcon from '@mui/icons-material/PermMedia';
+import LabelIcon from '@mui/icons-material/Label';
+import EmojiEmotionsIcon from '@mui/icons-material/EmojiEmotions';
+import LocationOnIcon from '@mui/icons-material/LocationOn';
+
+export default function Share() {
+  return (
+    <div className="share">
+      <div className="shareWrapper">
+        <div className="shareTop">
+          <img className="shareProfileImg" src="/assets/person/1.jpeg" alt="" />
+          <input
+            placeholder="What's in your mind Safak?"
+            className="shareInput"
+          />
+        </div>
+        <hr className="shareHr"/>
+        <div className="shareBottom">
+            <div className="shareOptions">
+                <div className="shareOption">
+                    <PermMediaIcon htmlColor="tomato" className="shareIcon"/>
+                    <span className="shareOptionText">Photo or Video</span>
+                </div>
+                <div className="shareOption">
+                    <LabelIcon htmlColor="blue" className="shareIcon"/>
+                    <span className="shareOptionText">Tag</span>
+                </div>
+                <div className="shareOption">
+                    <LocationOnIcon htmlColor="green" className="shareIcon"/>
+                    <span className="shareOptionText">Location</span>
+                </div>
+                <div className="shareOption">
+                    <EmojiEmotionsIcon htmlColor="goldenrod" className="shareIcon"/>
+                    <span className="shareOptionText">Feelings</span>
+                </div>
+            </div>
+            <button className="shareButton">Share</button>
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/pages/App.js
+++ src/pages/App.js
@@ -1,4 +1,4 @@
-import Home from "./pages/Home/Home";
+import Home from "./pages/home/Home";
 import Login from "./pages/login/Login";
 import Profile from "./pages/profile/Profile";
 import Register from "./pages/register/Register";
